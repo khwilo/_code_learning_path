d59d8853fcd6a6677f426a0dfc17111f11d3ca00
Khwilo <kwhyzzy@gmail.com>
2016-07-17 20:42:37 +0300

    add exercise 5

---
+++ python/lpthw/ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Khwilo W. Kabaka'
+my_age = 23
+my_height = 68 # inches
+my_weight = 72 # kgs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d kgs heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depenfing on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
