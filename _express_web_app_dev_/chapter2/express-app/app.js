8bf131ad76b6a86b8c851886867fb1e40726b8b5
Khwilo <kwhyzzy@gmail.com>
2016-08-23 10:35:01 +0300

    include jade and express in app

--- _express_web_app_dev_/chapter2/express-app/app.js
+++ _express_web_app_dev_/chapter2/express-app/app.js
@@ -5,12 +5,26 @@ var express = require('express');
 // create an instance of Express
 var app = express();
 
-// start the app
-http.createServer(app).listen(3000, function () {
-    console.log('Express app started');
-});
+// set the view engine
+app.set('view engine', 'jade');
+// locate the view files
+app.set('views', './views');
 
 // A route for the home page
 app.get('/', function (req, res){
-    res.send('<h1>Welcome!</h1>');
+    res.render('index');
+});
+
+// A route for /say-hello
+app.get('/say-hello', function (req,res) {
+    res.render('hello');
+});
+
+app.get('/test', function (req, res) {
+    res.send('this is a test');
+});
+
+// start the app
+http.createServer(app).listen(3000, function () {
+    console.log('Express app started');
 });
