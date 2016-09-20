a15806c7fe1273d1c080ae995af25bab7202447a
Khwilo <kwhyzzy@gmail.com>
2016-09-20 06:35:09 +0300

    add exercise 5

---
+++ ruby/lrthw/ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Khwilo W. Kabaka'
+my_age = 22
+my_height = 74 #inches
+my_weight = 72 #kgs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} kilograms heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
