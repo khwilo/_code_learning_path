bf45f60cab738d6e3676dedc366d2ec7412de523
khwilo <kwhyzzy@gmail.com>
2016-10-31 09:02:13 +0300

    start the test suite

--- ruby/tdd/unicorn/unicorn_test.rb
+++ ruby/tdd/unicorn/unicorn_test.rb
@@ -7,5 +7,16 @@ require 'minitest/autorun'
 # add pretty colors in the terminal when your run the tests
 require 'minitest/pride'
 
+# create a new test suite
+class UnicornTest < Minitest::Test
+	# add a test by adding a method whose name starts with test
+	def test_something
+	end
 
+	def test_something_else
+	end
+
+	def test_yet_another_thing
+	end
+end
 
