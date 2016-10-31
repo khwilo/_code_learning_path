94a0bbb3c70958ba8d0fe1ce176e4e3f5154cd22
khwilo <kwhyzzy@gmail.com>
2016-10-31 08:56:27 +0300

    configure Minitest

---
+++ ruby/tdd/unicorn/unicorn_test.rb
@@ -0,0 +1,11 @@
+# Use the minitest gem version installed in your system
+gem 'minitest', '~> 5.8'
+
+# load gem
+require 'minitest/autorun'
+
+# add pretty colors in the terminal when your run the tests
+require 'minitest/pride'
+
+
+
