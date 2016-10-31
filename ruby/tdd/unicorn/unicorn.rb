b83f790d6bc4f9c152d1f1b2c209298efc56de0b
khwilo <kwhyzzy@gmail.com>
2016-10-31 09:49:46 +0300

    add a triangulation test

--- ruby/tdd/unicorn/unicorn.rb
+++ ruby/tdd/unicorn/unicorn.rb
@@ -1,8 +1,11 @@
 class Unicorn
 	def initialize(name)
+		# save the name in @name
+		@name = name
 	end
 
 	def name
-		"Robert"
+		# return the instance variable from the initialize method
+		@name
 	end
 end
--- ruby/tdd/unicorn/unicorn_test.rb
+++ ruby/tdd/unicorn/unicorn_test.rb
@@ -13,12 +13,19 @@ require_relative 'unicorn'
 
 # create a new test suite
 class UnicornTest < Minitest::Test
+	# this test makes all unicorns to be called Robert
 	def test_it_has_a_name
 		# create a new unicorn
 		unicorn = Unicorn.new("Robert")
 
 		# make an assertion
 		assert_equal "Robert", unicorn.name
+	end
+
+	# this test triangulates the behaviour we want / expect
+	def test_it_can_be_named_somethig_else
+		unicorn = Unicorn.new("Joseph")
+		assert_equal "Joseph", unicorn.name
 	end
 end
 
