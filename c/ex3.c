eae45b763a8b882009b0d17e0756f8873723f095
khwilo <kwhyzzy@gmail.com>
2016-12-18 10:08:19 +0300

    re-do first 2 exercises

--- c/ex3.c
+++ c/ex3.c
@@ -2,11 +2,12 @@
 
 int main()
 {
-  int age = 23;
-  int height = 72;
+	int age = 10;
+	int height = 72;
 
-  printf("I am %d years old. \n", age);
-  printf("I am %d inches tall. \n", height);
- 
-  return 0;
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+
+	return 0;
 }
+
