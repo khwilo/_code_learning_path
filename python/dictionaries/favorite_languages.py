8e685cf4b36a6dc9be32502733e3d1983eeacf3d
khwilo <kwhyzzy@gmail.com>
2016-12-19 17:15:26 +0300

    include an ordered dictionary

--- python/dictionaries/favorite_languages.py
+++ python/dictionaries/favorite_languages.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 favorite_languages = {
         'jen': ['python', 'ruby'],
         'sarah': ['c'],
@@ -9,3 +11,17 @@ for name, languages in favorite_languages.items():
     print("\n" + name.title() + "'s favorite languages are:")
     for language in languages:
         print("\t" + language.title())
+
+
+favorited_languages = OrderedDict()
+
+favorited_languages['jen'] = 'python'
+favorited_languages['sarah'] = 'c'
+favorited_languages['edward'] = 'ruby'
+favorited_languages['phil'] = 'python'
+
+print("\n")
+
+for name, language in favorited_languages.items():
+    print(name.title() + "'s favorited language is " + 
+            language.title() + ".")
